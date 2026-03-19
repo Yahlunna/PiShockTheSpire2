a038d533dbd49afb9951bdaefc8ec9223c16508c
Yahlunna <jomontger@gmail.com>
2026-03-19 22:51:52 +0000

    Fixed relic texture pathing manually.

--- PiShockTheSpire2Code/Cards/Safeword.cs
+++ PiShockTheSpire2Code/Cards/Safeword.cs
@@ -1,26 +1,37 @@
 using BaseLib.Abstracts;
+using BaseLib.Extensions;
 using BaseLib.Utils;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using PiShockTheSpire2.PiShockTheSpire2Code.Utils;
 
 namespace PiShockTheSpire2.PiShockTheSpire2Code.Cards;
-
   
 [Pool(typeof(TokenCardPool))]
 public class Safeword() : CustomCardModel(0, CardType.Skill,
     CardRarity.Basic, TargetType.Self)
 {
+    public override string CustomPortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigCardImagePath();
+    public override string PortraitPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".CardImagePath();
+
+    //public override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<Mercy Power>(1)];  Create Power: Mercy
+    //public override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<Mercy Power>()]; Define Mercy
+    
+    
     public override async Task OnPlay(
         PlayerChoiceContext choiceContext,
         CardPlay play)
     {
-        
+        // Add Mercy.
     }
 
     public override void OnUpgrade()
     {
-
+        //DynamicVars.Power.UpgradeValueBy(1);  (Im just guessing it will be something like this but i honestly have no idea lmao.)
     }
     
     
--- PiShockTheSpire2Code/Relics/PishockCollar.cs
+++ PiShockTheSpire2Code/Relics/PishockCollar.cs
@@ -1,7 +1,10 @@
 using BaseLib.Abstracts;
+using BaseLib.Extensions;
 using BaseLib.Utils;
+using Godot;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.Models.RelicPools;
+using PiShockTheSpire2.PiShockTheSpire2Code.Utils;
 
 namespace PiShockTheSpire2.PiShockTheSpire2Code.Relics;
 
@@ -11,7 +14,42 @@ public class PishockCollar() : CustomRelicModel
     public override RelicRarity Rarity =>
         RelicRarity.Starter;
 
+    // TODO: Sanitize and generalize this.
+    public override string PackedIconPath => "pishockcollar.png".RelicImagePath();
+    public override string PackedIconOutlinePath => "pishockcollar_outline.png".RelicImagePath();
+    public override string BigIconPath => "pishockcollar.png".BigRelicImagePath();
     
     
-    
+    /*
+    public override string PackedIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".RelicImagePath();
+            return ResourceLoader.Exists(path) ? path : "relic.png".RelicImagePath();
+        }
+    }
+
+    public override string PackedIconOutlinePath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}_outline.png".RelicImagePath();
+            return ResourceLoader.Exists(path) ? path : "relic_outline.png".RelicImagePath();
+        }
+    }
+
+    public override string BigIconPath
+    {
+        get
+        {
+            var path = $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigRelicImagePath();
+            return ResourceLoader.Exists(path) ? path : "relic.png".BigRelicImagePath();
+        }
+    }
+    */
+
+
+
+
 }
