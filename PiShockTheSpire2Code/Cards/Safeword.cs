160e171de32b7c1ed2f076d8bd96bb1cbcea0323
Yahlunna <jomontger@gmail.com>
2026-03-19 17:57:58 +0000

    Adding Safeword card template.

---
+++ PiShockTheSpire2Code/Cards/Safeword.cs
@@ -0,0 +1,29 @@
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+namespace PiShockTheSpire2.PiShockTheSpire2Code.Cards;
+
+  
+[Pool(typeof(IroncladCardPool))]
+public class Safeword() : CustomCardModel(0, CardType.Skill,
+    CardRarity.Basic, TargetType.Self)
+{
+    public override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        
+    }
+
+    public override void OnUpgrade()
+    {
+
+    }
+    
+    
+    
+    
+}
